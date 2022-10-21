cdd85f74c44ee29b6db0950d69096f06c7e134d3
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-21 16:23:28 -0700

    forgot to save

--- components/navBar.js
+++ components/navBar.js
@@ -5,8 +5,7 @@ export default function NavBar(){
     return(
         <nav>
             <Link href='/'>Home</Link>            
-            <Link href='/about'>About Me</Link>            
-
+            <Link href='/about'>About Me</Link>
         </nav>
     )
 }
