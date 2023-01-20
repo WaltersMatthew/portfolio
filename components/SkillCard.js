ae6bc13b74f01b575023f255af87c7715d675561
waltersmatthew <waltersmatthew88@gmail.com>
2023-01-20 11:03:19 -0800

    remove drop shadow from skillcards

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -4,11 +4,11 @@ export default function SkillCard(props) {
     return (
         <div className="mx-auto flex flex-col">
             <Image
-                className="rounded-full shadow-lg"
+                className="rounded-full"
                 src={props.src}
                 alt={props.alt}
                 height="90"
-                width="100"
+                width="95"
             />
             <p className="mx-auto pt-2 text-lg">{props.skill}</p>
         </div>
